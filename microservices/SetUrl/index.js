338763b16c771c53827f1fcda9ea9c73e9e4b235
adhyyanT <adhyyan321@gmail.com>
2023-07-12 21:06:40 +0530

    fixed typos

--- microservices/SetUrl/index.js
+++ microservices/SetUrl/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/database');
-const Pair = require('./Models/Pair');
+const Pair = require('./models/Pair');
 require('dotenv').config;
 const cors = require('cors');
 const app = express();
